0104c3ab851358b17e20c2c9e31f57f41ddab762
jeet.inani@westpac.com.au <jeet.inani@westpac.com.au>
2024-02-15 22:09:04 +0530

    base commit

---
+++ src/main/java/com/inani/bank/InaniBankApplication.java
@@ -0,0 +1,12 @@
+package com.inani.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InaniBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InaniBankApplication.class, args);
+	}
+}
---
+++ src/test/java/com/inani/bank/InaniBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inani.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InaniBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
