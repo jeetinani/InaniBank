6878715e07b6857d584bca8c8675c8bc03d96754
Jeet Inani <jeetinani@gmail.com>
2024-09-01 12:28:12 +0530

    full containerization

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -9,7 +9,6 @@ import Layout from "./components/Layout";
 import LoggingPage from './components/LoggingPage';
 import Logout from "./components/Logout";
 import { useState, useEffect } from 'react';
-import links from "./links.json";
 
 function App() {
 
@@ -18,8 +17,22 @@ function App() {
   let token = window.sessionStorage.getItem("user-info");
   useEffect(() => {
     console.log(`in useEffect of app.js`)
-    console.log('host is ' + links.host);
-    axios.defaults.baseURL = `http://${links.host}`;
+    //console.log('host is ' + links.host);
+    /* fetch('/links.json')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Fetched links.json:', data);
+        const host = process.env.REACT_APP_BACKEND_URL || `http://${data.host}`;
+        console.log(`Setting axios baseURL to ${host}`);
+        axios.defaults.baseURL = host;
+      })
+      .catch(error => {
+        console.error('Error fetching links.json:', error);
+      }); */
+    //const host = process.env.REACT_APP_BACKEND_URL || `http://${links.host}`;;
+    //console.log(`Setting axios baseURL to ${host}`);
+    //axios.defaults.baseURL = host;
+    //axios.defaults.baseURL = `http://${links.host}`;
     if (token) {
       let options = {
         headers: {
